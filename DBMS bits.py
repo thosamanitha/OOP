6aa8ea83b45bea3d11f8404ead6b3bb4dd944ce2
GHarshitha115 <54620828+GHarshitha115@users.noreply.github.com>
2020-04-28 16:36:25 +0530

    Create DBMS bits.py

---
+++ DBMS bits.py
@@ -0,0 +1,16 @@
+which of the following scenarios leads to data integrity problems?
+answer:Writing a string value for a field where integer is expected
+if a machine crashes while a program is updating a record?
+answer:Durability
+A DBMS is a?
+answer:collection of programs to access data
+Relationship among a set of values?
+answer:a table
+Related to data redundancy?
+answer:Storing album and artists details in same table where album can have more than one artist
+a relational database is collection of?
+answer:tables
+DBMS functions:
+1.Creating Databases
+2.Retrieving data
+3.Administrating databases
